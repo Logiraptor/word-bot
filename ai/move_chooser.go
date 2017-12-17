5f2b7be52734af648bdbe5cb03e55e8f7aab525e
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-12-17 18:01:07 -0500

    Fix compile error in webpack build

--- ai/move_chooser.go
+++ ai/move_chooser.go
@@ -19,14 +19,12 @@ func NewMoveChooser(name string, gen MoveGenerator, eval MoveEvaluator) *MoveCho
 var _ AI = &MoveChooser{}
 
 func (m *MoveChooser) FindMove(b *core.Board, bag core.Bag, rack core.Rack, onMove func(core.Turn) bool) {
-	var bestMove core.ScoredMove
 	var bestScore float64
 	m.generator.GenerateMoves(b, rack, func(t core.Turn) bool {
 		if sm, ok := t.(core.ScoredMove); ok {
 			score := m.evaluator.Evaluate(b, rack, sm)
 			if score > bestScore {
 				bestScore = score
-				bestMove = sm
 				return onMove(sm)
 			}
 		}
